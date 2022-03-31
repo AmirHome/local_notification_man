db6c25887c2ff8f32eec84e1d933aa4c70f4e203
amirhome <amir.email@yahoo.com>
2022-03-31 11:51:38 +0300

    show and schedule  notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,83 @@
 import 'package:flutter/material.dart';
+import 'notification_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  NotificationService().init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: Colors.blue,
+          accentColor: Colors.indigo.shade900,
+          appBarTheme: AppBarTheme(color: Colors.indigo.shade900)),
+      home: MyHomePage(title: 'Flutter Local Notifications'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  NotificationService _notificationService = NotificationService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  RaisedButton(
+                    child: Text('Show Notification'),
+                    padding: const EdgeInsets.all(10),
+                    onPressed: () async {
+                      await _notificationService.showNotifications();
+                    },
+                  ),
+                  SizedBox(height: 3),
+                  RaisedButton(
+                    child: Text('Schedule Notification'),
+                    padding: const EdgeInsets.all(10),
+                    onPressed: () async {
+                      await _notificationService.scheduleNotifications();
+                    },
+                  ),
+                  SizedBox(height: 3),
+                  RaisedButton(
+                    child: Text('Cancel Notification'),
+                    padding: const EdgeInsets.all(10),
+                    onPressed: () async {
+                      await _notificationService.cancelNotifications(0);
+                    },
+                  ),
+                  SizedBox(height: 3),
+                  RaisedButton(
+                    child: Text('Cancel All Notifications'),
+                    padding: const EdgeInsets.all(10),
+                    onPressed: () async {
+                      await _notificationService.cancelAllNotifications();
+                    },
+                  ),
+                  SizedBox(height: 3),
+                ],
+              ),
+            )));
   }
 }
